98e14526ea4689b7cfb0adebb2525d5724600a07
seunghyun <seunghyun937@gmail.com>
2026-02-02 18:35:14 +0900

    Feat: 새로운 페이지 라우팅 추가
    About, 비밀번호 재설정, OAuth 콜백, 내 카드, 통계, 설정,
    개인정보처리방침, 이용약관, 관리자 카드 관리 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from '@/contexts/AuthContext'
 import { HomePage } from '@/pages/HomePage'
+import { AboutPage } from '@/pages/AboutPage'
 import { MyPage } from '@/pages/MyPage'
 import { StudyPage } from '@/pages/StudyPage'
 import { LoginPage } from '@/pages/LoginPage'
 import { SignupPage } from '@/pages/SignupPage'
+import { ForgotPasswordPage } from '@/pages/ForgotPasswordPage'
+import { StatsPage } from '@/pages/StatsPage'
+import { OAuthCallbackPage } from '@/pages/OAuthCallbackPage'
+import { PrivacyPage } from '@/pages/PrivacyPage'
+import { TermsPage } from '@/pages/TermsPage'
+import { MyCardsPage } from '@/pages/MyCardsPage'
+import { SettingsPage } from '@/pages/SettingsPage'
+import { AdminCardsPage } from '@/pages/AdminCardsPage'
+import { AdminRoute } from '@/components/AdminRoute'
 
 function AppRoutes() {
   return (
     <AuthProvider>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/oauth/callback" element={<OAuthCallbackPage />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/my-cards" element={<MyCardsPage />} />
         <Route path="/study" element={<StudyPage />} />
+        <Route path="/stats" element={<StatsPage />} />
+        <Route path="/privacy" element={<PrivacyPage />} />
+        <Route path="/terms" element={<TermsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/admin/cards"
+          element={
+            <AdminRoute>
+              <AdminCardsPage />
+            </AdminRoute>
+          }
+        />
       </Routes>
     </AuthProvider>
   )
